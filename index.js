de5982d45d6ba85434d6a891b4afa266c0131f09
brian <brian.ogango@student.moringaschool.com>
2022-11-23 20:01:50 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function divide(num1,num2) {
+   return 2000/100;
+}
+const square =(parameter2) => parameter2*parameter2;
+square(2);
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,5); 
